cafa57b209011177e954f57100ac4d3c0ac51a16
rscheurman <riley.scheurman@gmail.com>
2018-08-14 13:33:14 -0600

    initial commit - fb auth (pre-built UI)

---
+++ public/js/firebase.js
@@ -0,0 +1,16 @@
+var app_firebase = {};
+(function(){
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDOuUtXp_i2TcjlLWv97lEVznJ-X6WiatA",
+  authDomain: "tasktracker-c8e8d.firebaseapp.com",
+  databaseURL: "https://tasktracker-c8e8d.firebaseio.com",
+  projectId: "tasktracker-c8e8d",
+  storageBucket: "tasktracker-c8e8d.appspot.com",
+  messagingSenderId: "593526783184"
+};
+firebase.initializeApp(config);
+
+
+
+})();
