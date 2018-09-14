71c53f07868c90461c6e91a8a21bfcbc4ce8931c
rscheurman <riley.scheurman@gmail.com>
2018-09-13 22:47:20 -0600

    - Fixed code formatting from previous integration

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,6 @@
 var mainApp = {};
 
 (function() {
-<<<<<<< HEAD
-  var uid = null;
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      uid = user.uid;
-      email = user.email;
-      name = user.displayName;
-      writeUserData(name, uid, email);
-      $("#login").hide();
-      $("#logout").show();
-      console.log("Sign in confirmed");
-      // toastNotif(name);
-    } else {
-      $(".tasks").hide();
-      uid = null;
-      // Redirect to login page
-      //   window.location.replace("auth.html");
-      $("#logout").hide();
-      $("#login").show();
-      console.log("no user logged in");
-    }
-  });
-  // Login functionality
-  const loginBtn = document.getElementById("login");
-  loginBtn.addEventListener("click", e => {
-    window.location.replace("auth.html");
-  });
-=======
 	var uid = null;
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
@@ -49,7 +21,6 @@ var mainApp = {};
 			$('#login').show();
 			console.log('no user logged in');
 		}
->>>>>>> impCloudFirestore
 
 		// Login functionality
 		const loginBtn = document.getElementById('login');
@@ -57,33 +28,11 @@ var mainApp = {};
 			window.location.replace('auth.html');
 		});
 
-<<<<<<< HEAD
-  // Toast Notifications
-  function toastNotif(name) {
-    M.toast({
-      classes: "idToast",
-      html: "Welcome, " + name,
-      completeCallback: hideNotif()
-    });
-  }
-
-  // Hide Notification after displayed
-  function hideNotif() {
-    $(".idToast");
-  }
-
-  //  Realtime Database
-  function writeUserData(name, uid, email) {
-    var database = firebase.database();
-    var usersRef = database.ref("users");
-    var user = usersRef.child(uid);
-=======
 		//  Logout functinoality
 		const logoutBtn = document.getElementById('logout');
 		logoutBtn.addEventListener('click', (e) => {
 			firebase.auth().signOut();
 		});
->>>>>>> impCloudFirestore
 
 		// Toast Notifications
 		function toastNotif(name) {
--- public/js/tasksDB.js
+++ public/js/tasksDB.js
@@ -1,17 +1,4 @@
 (function() {
-<<<<<<< HEAD
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      var uid = user.uid;
-    } else {
-      // No user is signed in.
-    }
-		
-		
-
-		
-  });
-=======
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
 			// User is signed in
@@ -79,5 +66,4 @@
 			});
 		});
 	});
->>>>>>> impCloudFirestore
 })();
